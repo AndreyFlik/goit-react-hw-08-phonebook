eb81a7b870d0049caf37d0d887e8e54b69e8c18b
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2022-01-08 22:27:58 +0200

    добавил lazy

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { NavLink, Route, Switch } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import Contacts from "../Contacts/Contacts";
-import Register from "../Register/Register";
-import Login from "../Login/Login";
+// import Contacts from "../Contacts/Contacts";
+// import Register from "../Register/Register";
+// import Login from "../Login/Login";
 import UserMenu from "../UserMenu/UserMenu";
-import StartPage from "../StartPage/StartPage";
+// import StartPage from "../StartPage/StartPage";
 import PrivateRoute from "../PrivateRoute";
 import PublicRoute from "../PublicRoute";
 import { getCurrentUser } from "../../redux/contacts/contscts-operations";
+
+const Contacts = lazy(() =>
+  import("../Contacts/Contacts" /* webpackChunkName: "ContactsPage"*/)
+);
+const Register = lazy(() =>
+  import("../Register/Register" /* webpackChunkName: "RegisterPage"*/)
+);
+const Login = lazy(() =>
+  import("../Login/Login" /* webpackChunkName: "LoginPage"*/)
+);
+// const UserMenu = lazy(() =>
+//   import("../UserMenu/UserMenu" /* webpackChunkName: "UserMenu"*/)
+// );
+const StartPage = lazy(() =>
+  import("../StartPage/StartPage" /* webpackChunkName: "StartPage"*/)
+);
 
 const App = () => {
   const dispatch = useDispatch();
@@ -42,20 +58,22 @@ const App = () => {
         </nav>
       )}
       {!loading && (
-        <Switch>
-          <PublicRoute restricted redirectTo="/Contacts" path="/Login">
-            <Login />
-          </PublicRoute>
-          <PublicRoute restricted path="/Register">
-            <Register />
-          </PublicRoute>
-          <PrivateRoute restricted redirectTo="/Login" path="/Contacts">
-            <Contacts />
-          </PrivateRoute>
-          <Route path="/">
-            <StartPage />
-          </Route>
-        </Switch>
+        <Suspense fallback={<h1>Загрузка</h1>}>
+          <Switch>
+            <PublicRoute restricted redirectTo="/Contacts" path="/Login">
+              <Login />
+            </PublicRoute>
+            <PublicRoute restricted path="/Register">
+              <Register />
+            </PublicRoute>
+            <PrivateRoute restricted redirectTo="/Login" path="/Contacts">
+              <Contacts />
+            </PrivateRoute>
+            <Route path="/">
+              <StartPage />
+            </Route>
+          </Switch>
+        </Suspense>
       )}
     </>
   );
